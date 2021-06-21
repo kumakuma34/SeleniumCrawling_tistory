8e3d5ad2cb5df97a7a2f4cdecaec9fe2e7470b57
kumakuma34 <qgqg264@gmail.com>
2021-06-21 14:08:21 +0900

    feat: PI check
    Signed-off-by: kumakuma34 <qgqg264@gmail.com>

--- onlyonefoodnet_crawling_test.py
+++ onlyonefoodnet_crawling_test.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import config
 import time
-driver = webdriver.Chrome("/Users/hyunsoojang/Downloads/chromedriver")
+driver = webdriver.Chrome()
 driver.implicitly_wait(5)
 
 def check_exist_by_xpath(xpath):
@@ -44,12 +47,21 @@ driver.find_element_by_id('pop_userChange_btnChoice').click()
 time.sleep(3)
 driver.switch_to.frame(driver.find_element_by_tag_name('iframe'))
 
-#결제예상금액 조회 - PI 통
-time.sleep(3)
-driver.find_element_by_xpath('//*[@id="mainSttlSearchBtn"]').send_keys(Keys.ENTER)
-pi_success = check_exist_by_id('sttlScdAt')
-pi_element = driver.find_element_by_id('sttlScdAt')
-print(str(pi_success))
-print(pi_element.text)
+#결제예상금액 조회 - PI 통신
+print('결제 예상금액 조회 결과(PI 통신 결과)')
+try:
+    element = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.ID, "mainSttlSearchBtn"))
+        )
+    element.click()
+    time.sleep(5)
+    if  driver.find_element_by_id('sttlScdAt').text == "0":
+        print("False / 조회 오류")
+    else:
+        print("True / 결제 예상금액 : " + driver.find_element_by_id('sttlScdAt').text)
+except:
+    print("False / 조회 오류")
+
+
 
 
